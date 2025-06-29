25344648cf762cc0f0daa04050b22ca347e86ef6
arindam3122 <ronimitra166@gmail.com>
2025-06-29 13:41:42 +0530

    delete

---
+++ quizData.js
@@ -0,0 +1,97 @@
+// quizData.js
+
+const quizzes = [
+    {
+        id: "cls-6",
+        name: "Logical Reasoning (PART-1)",
+        description: "Easy wordproblems",
+        enabled: true,
+        questions: [
+            {
+                question: "Rita has 348 apples. She sells 125 apples. How many apples are left with her? (ANSWER THE NUMBER ONLY)",
+                type: "input",
+                answer: "223",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "A packet has 24 chocolates. How many chocolates are there in 15 such packets? (NUMBER ONLY)",
+                type: "input",
+                answer: "360",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "There are 480 mangoes packed equally in 12 boxes. How many mangoes are there in each box? (NUMBERS ONLY)",
+                type: "input",
+                answer: "40",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "A movie starts at 3:15 PM and ends at 5:45PM. How long is the movie? (Answer in format a hours b min)",
+                type: "input",
+                answer: "2 hours 30 min",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "Ankit has ₹500. He buys a book for ₹275. How much money is left with him? (Answer without any units)",
+                type: "input",
+                answer: "225",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "The ages of 4 children are 10, 12, 13 and 15 years. Find their average age.",
+                type: "input",
+                answer: "12.5",
+                imageUrl: "",
+                timeLimit: 180
+            },
+        ]
+    },
+    {
+        id: "cls-10",
+        name: "Logical Reasoning (PART-2)",
+        description: "Medium Wordproblems",
+        enabled: true,
+        questions: [
+            {
+                question: "The sum of two numbers is 50. One number is 8 more than the other. Find the numbers. (Format a and b)",
+                type: "input",
+                answer: "21 and 29",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "A rectangular plot has an area of 180m2. Its length is 5m more than its width. Find its dimentions.(Write as a,b where a=width and b=length)",
+                type: "input",
+                answer: "12,17",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "",
+                type: "input",
+                answer: "",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "",
+                type: "input",
+                answer: "",
+                imageUrl: "",
+                timeLimit: 180
+            },
+            {
+                question: "",
+                type: "input",
+                answer: "",
+                imageUrl: "",
+                timeLimit: 180
+            },
+        ]
+    },
+];
