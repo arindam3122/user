f9a3c4cae25d6a9d11d4f0e934c6a7575775f3fe
arindam3122 <ronimitra166@gmail.com>
2025-09-07 07:28:25 +0530

    oii

--- quizData.js
+++ quizData.js
@@ -1,5 +1,22 @@
 // mergedQuizData.js
 const quizzes = [
   
+    {
+        id: "rat",
+        name: "",
+        description: "",
+        enabled: false,
+        questions: [
+            {
+                question: "dad",
+                type: "input",
+                options: [],
+                answer: "dad",
+                timeLimit: 30,
+                imageUrl: "",
+                explanationImageUrl: ""
+            }
+        ]
+    }
 
 ];
